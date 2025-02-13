27fa3f0090b6fb068ca75cc6afd93da1402f5039
CodeBytes <79523926+CodeBytes94@users.noreply.github.com>
2025-02-13 19:04:46 +0000

    Create 02-syntax-variables-data-types.py

---
+++ 02-syntax-variables-data-types/02-syntax-variables-data-types.py
@@ -0,0 +1,38 @@
+# Creating variables and storing information
+name = "Jasmine"
+age = 30
+is_member = True
+
+# Outputting the variables to see the stored information
+print(name)
+print(age)
+print(is_member)
+
+# Reassignment of variables
+is_member = False - Reassignment of variable
+print(is_member) - Checking reassignment of variable
+
+# 123name = "Jasmine" - Error because variables cannot start with a number
+# my-name = "Jasmine" - Error because variables must use underscores not dashes (snake case)
+
+# Checking data type of variables
+greeting = "Welcome to CodeBytes"
+print(greeting)
+print(type(greeting)) # string data type
+
+age = 30
+price = 19.99
+
+print(age)
+print(type(age)) # integer data type
+
+print(price)
+print(type(price)) # float data type
+
+is_member = False
+print(is_member)
+print(type(is_member)) # boolean data type
+
+
+
+
